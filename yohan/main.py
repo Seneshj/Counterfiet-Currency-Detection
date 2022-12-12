4675a66fffd4998400e56f951cc01d20286a28c9
yohanAbhishek <yohan.20200509@iit.ac.lk>
2022-12-12 08:56:42 +0530

    Changed directory of yohan//etc to etc

--- yohan/main.py
+++ yohan/main.py
@@ -2,29 +2,27 @@
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.optimizers import RMSprop
-import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
-import pickle
-import cv2
 import os
 
+cwd = os.getcwd()
+print(cwd)
 
-train = ImageDataGenerator(rescale=1/255)
-validation =  ImageDataGenerator(rescale=1/255)
+train = ImageDataGenerator(rescale=1 / 255)
+validation = ImageDataGenerator(rescale=1 / 255)
 
 train_dataset = train.flow_from_directory(
-    "yohan\\train", 
-    target_size=(200,200), 
-    batch_size = 3,   
+    "train",
+    target_size=(200, 200),
+    batch_size=3,
     class_mode="binary"
 )
 
 validate_dataset = train.flow_from_directory(
-    "yohan\\validate", 
-    target_size=(200,200), 
-    batch_size = 3, 
+    "validate",
+    target_size=(200, 200),
+    batch_size=3,
     class_mode="binary"
 )
 
@@ -34,15 +32,15 @@ print(validate_dataset.class_indices)
 model = tf.keras.models.Sequential(
     [
         # Layer1
-        tf.keras.layers.Conv2D(filters=16, kernel_size=(3,3), activation='relu', input_shape=(200,200,3)), 
-        tf.keras.layers.MaxPool2D(pool_size=2, strides=2), # MaxPool takes the max pixel out of given pixels
+        tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3), activation='relu', input_shape=(200, 200, 3)),
+        tf.keras.layers.MaxPool2D(pool_size=2, strides=2),  # MaxPool takes the max pixel out of given pixels
 
         # Layer2
-        tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3), activation='relu'), 
+        tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu'),
         tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
 
         # Layer3
-        tf.keras.layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'), 
+        tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
         tf.keras.layers.MaxPool2D(pool_size=2, strides=2),
 
         # Flatten
@@ -50,35 +48,32 @@ model = tf.keras.models.Sequential(
 
         # Apply 2 dense layers
         tf.keras.layers.Dense(512, activation='relu'),
-        tf.keras.layers.Dense(1, activation='sigmoid'), # sigmoid becuase program is binary (500 or 1000)
+        tf.keras.layers.Dense(1, activation='sigmoid'),  # sigmoid becuase program is binary (500 or 1000)
     ]
 )
 
 # Compile model
-model.compile(loss= 'binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Train model
 model_fit = model.fit(
-    train_dataset, 
-    steps_per_epoch=3, 
-    epochs=7, 
-    validation_data=validate_dataset) # Epoch is the number of iterations to run compile model
+    train_dataset,
+    steps_per_epoch=3,
+    epochs=7,
+    validation_data=validate_dataset)  # Epoch is the number of iterations to run compile model
 
 # Test model classification 
-dir_path = "yohan\\test"
+dir_path = "test"
 
-
-img = image.load_img("yohan\\test\\Five Thousand Rupee Real.jpg", target_size=(200,200))
+img = image.load_img("test\\Five Thousand Rupee Real.jpg", target_size=(200, 200))
 
 X = image.img_to_array(img)
 X = np.expand_dims(X, axis=0)
 images = np.vstack([X])
 val = model.predict(images)
 
-
 ## In validate_dataset.class_indices Fake = 0, and Real = 1
 if val == 0:
     print(f"Fake")
 else:
     print(f"Real")
-        
