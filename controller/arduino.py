c4b02a3e45ce88fb2796ba32924ca52c02eaacc3
yohanAbhishek <yohan.20200509@iit.ac.lk>
2023-01-25 22:20:24 +0530

    Created method to turn on light

--- controller/arduino.py
+++ controller/arduino.py
@@ -2,13 +2,10 @@ import serial
 
 ser = serial.Serial('COM6', 9600)  # open serial port
 
-while True:
-    user_input = input("Enter '1' to turn on LED or '0' to off: ")
-    if user_input == '1':
-        ser.write(b'1')  # send '1' over serial to turn on LED
-    elif user_input == '0':
-        ser.write(b'0')
-    else:
-        print("Invalid input. Please enter '1' or '0'.")
 
-ser.close()  # close serial port
+def on_uv_light():
+    ser.write(b'1')  # send '1' over serial to turn on LED
+
+
+def off_uv_light():
+    ser.write(b'0')
