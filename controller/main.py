b1e9bd0061e04cd8df82f3a6cf793a46504b7903
Nadeem Nishaam <119834836+nadeemnisham@users.noreply.github.com>
2023-02-03 16:07:07 +0530

    Created the data and split to be used by the background cropping model

--- controller/main.py
+++ controller/main.py
@@ -1,8 +1,10 @@
 import arduino
 
+
 user_input = input("--Select an option from below--\n1.Detect currency\n2.Identify fake\n->")
 
 if user_input == "1":
     pass
 elif user_input == "2":
     arduino.on_uv_light()
+
---
+++ model/crop_image.py
@@ -0,0 +1,110 @@
+import cv2
+import tensorflow as tf
+import numpy as np
+import os
+import json
+import albumentations as alb
+
+
+def load_image(x):
+    byte_img = tf.io.read_file(x)
+    img = tf.io.decode_jpeg(byte_img)
+    return img
+
+
+def load_labels(label_path):
+    with open(label_path.numpy(), 'r', encoding="utf-8") as f:
+        label = json.load(f)
+
+    return [label['class']], label['bbox']
+
+augmentor = alb.Compose([alb.HorizontalFlip(p=0.5),
+                         alb.RandomBrightnessContrast(p=0.2),
+                         alb.RandomGamma(p=0.2),
+                         alb.RGBShift(p=0.2),
+                         alb.VerticalFlip(p=0.5)],
+                        bbox_params=alb.BboxParams(format='albumentations',
+                                                   label_fields=['class_labels']))
+
+for partition in ['train', 'test', 'val']:
+    for image in os.listdir(os.path.join('data', partition, 'images')):
+        img = cv2.imread(os.path.join('data', partition, 'images', image))
+
+        coords = [0, 0, 0.00001, 0.00001]
+        label_path = os.path.join('data', partition, 'labels', f'{image.split(".")[0]}.json')
+        if os.path.exists(label_path):
+            with open(label_path, 'r') as f:
+                label = json.load(f)
+
+            coords[0] = label['shapes'][0]['points'][0][0]
+            coords[1] = label['shapes'][0]['points'][0][1]
+            coords[2] = label['shapes'][0]['points'][1][0]
+            coords[3] = label['shapes'][0]['points'][1][1]
+            coords = list(np.divide(coords, [4000, 3000, 4000, 3000]))
+
+        try:
+            for x in range(60):
+                augmented = augmentor(image=img, bboxes=[coords], class_labels=['face'])
+                cv2.imwrite(os.path.join('aug_data', partition, 'images', f'{image.split(".")[0]}.{x}.jpg'),
+                            augmented['image'])
+
+                annotation = {}
+                annotation['image'] = image
+
+                if os.path.exists(label_path):
+                    if len(augmented['bboxes']) == 0:
+                        annotation['bbox'] = [0, 0, 0, 0]
+                        annotation['class'] = 0
+                    else:
+                        annotation['bbox'] = augmented['bboxes'][0]
+                        annotation['class'] = 1
+                else:
+                    annotation['bbox'] = [0, 0, 0, 0]
+                    annotation['class'] = 0
+
+                with open(os.path.join('aug_data', partition, 'labels', f'{image.split(".")[0]}.{x}.json'), 'w') as f:
+                    json.dump(annotation, f)
+
+        except Exception as e:
+            print(e)
+
+train_images = tf.data.Dataset.list_files('aug_data\\train\\images\\*.jpg', shuffle=False)
+train_images = train_images.map(load_image)
+train_images = train_images.map(lambda x: tf.image.resize(x, (120, 120)))
+train_images = train_images.map(lambda x: x / 255)
+
+test_images = tf.data.Dataset.list_files('aug_data\\test\\images\\*.jpg', shuffle=False)
+test_images = test_images.map(load_image)
+test_images = test_images.map(lambda x: tf.image.resize(x, (120, 120)))
+test_images = test_images.map(lambda x: x / 255)
+
+val_images = tf.data.Dataset.list_files('aug_data\\val\\images\\*.jpg', shuffle=False)
+val_images = val_images.map(load_image)
+val_images = val_images.map(lambda x: tf.image.resize(x, (120, 120)))
+val_images = val_images.map(lambda x: x / 255)
+
+train_labels = tf.data.Dataset.list_files('aug_data\\train\\labels\\*.json', shuffle=False)
+train_labels = train_labels.map(lambda x: tf.py_function(load_labels, [x], [tf.uint8, tf.float16]))
+
+test_labels = tf.data.Dataset.list_files('aug_data\\test\\labels\\*.json', shuffle=False)
+test_labels = test_labels.map(lambda x: tf.py_function(load_labels, [x], [tf.uint8, tf.float16]))
+
+val_labels = tf.data.Dataset.list_files('aug_data\\val\\labels\\*.json', shuffle=False)
+val_labels = val_labels.map(lambda x: tf.py_function(load_labels, [x], [tf.uint8, tf.float16]))
+
+train = tf.data.Dataset.zip((train_images, train_labels))
+train = train.shuffle(3000)
+train = train.batch(8)
+train = train.prefetch(4)
+
+test = tf.data.Dataset.zip((test_images, test_labels))
+test = test.shuffle(600)
+test = test.batch(8)
+test = test.prefetch(4)
+
+val = tf.data.Dataset.zip((val_images, val_labels))
+val = val.shuffle(600)
+val = val.batch(8)
+val = val.prefetch(4)
+
+
