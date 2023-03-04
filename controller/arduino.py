1d0a3bf1f7f9f4234f8039605fe66e0513012d0a
yohanAbhishek <yohan.20200509@iit.ac.lk>
2023-03-04 10:24:59 +0530

    Created methods to turn on and off UV

--- controller/arduino.py
+++ controller/arduino.py
@@ -7,14 +7,24 @@ ser = serial.Serial('COM6', 9600, timeout=1)
 # Wait for the Arduino to initialize
 time.sleep(2)
 
-while True:
-    # Read the value from the serial port and print it to the console
-    ldrValue = ser.readline().decode('utf-8').rstrip()
-    print("LDR value: ", ldrValue)
 
-    # Check for keyboard input and send command to Arduino
-    command = input("Enter command: ")
-    if command == 's':
-        ser.write(b's')
-    elif command == 'x':
-        ser.write(b'x')
+def uv_on():
+    ser.write(b's')
+
+
+def uv_off():
+    ser.write(b'x')
+
+
+# while True:
+#     # Read the value from the serial port and print it to the console
+#     ldrValue = ser.readline().decode('utf-8').rstrip()
+#     print("LDR value: ", ldrValue)
+#
+#     # Check for keyboard input and send command to Arduino
+#     command = input("Enter command: ")
+#     if command == 's':
+#         ser.write(b's')
+#     elif command == 'x':
+#         ser.write(b'x')
+
