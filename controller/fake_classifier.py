6722a9aa1cc9194b4ecbe85fd9e64b57a9c0a19e
yohanAbhishek <yohan.20200509@iit.ac.lk>
2023-03-04 14:11:31 +0530

    Changed model directory

--- controller/fake_classifier.py
+++ controller/fake_classifier.py
@@ -2,17 +2,20 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import cv2
+import arduino
 
 
 # note value is the value of the note as an int or string. Eg: 20,50,100,500,1000,5000
 def fake_classifier(note_value):
+    arduino.uv_on()
     cap = cv2.VideoCapture(0)
-    _,frame = cap.read()
+    _, frame = cap.read()
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     resized = tf.image.resize(rgb, (120, 120))
     # assuming the models are saved in the same file as the python file
-    model = keras.models.load_model('fake-' + str(note_value) + '.h5')
+    model = keras.models.load_model('../model/weights/fake-' + str(note_value) + '.h5')
     yhat = model.predict(np.expand_dims(resized / 255, 0))
+    arduino.uv_off()
     # returns a string saying real or fake
     if yhat[0] > 0.5:
         return 'Real'
--- controller/main.py
+++ controller/main.py
@@ -1,14 +1,20 @@
 import identify as i
-
+import fake_classifier as f
+import detect_note as d
 
 while True:
     option = int(input("Enter one of the below options: \n1. Identify currency\n2. Identify Counterfeit note\n3. Sum "
                        "up currency\n4. Exit\n\n->"))
     if option == 1:
-        i.main()
-        print(i.get_value())
+        if d.detect_note():
+            print("Image found")
+            i.main()
+            print(i.get_value())
     elif option == 2:
-        i.voice_out("Not done")
+        print("Image found")
+        if d.detect_note():
+            i.main()
+            i.voice_out(f.fake_classifier(i.get_value()))
     elif option == 3:
         i.voice_out("Note done")
     elif option == 4:
