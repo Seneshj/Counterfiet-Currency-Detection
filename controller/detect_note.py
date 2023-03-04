bee217697b131481f36fa4b32265a36e044c7980
yohanAbhishek <yohan.20200509@iit.ac.lk>
2023-03-04 21:12:58 +0530

    Temporarly commented detect_note.py

--- controller/detect_note.py
+++ controller/detect_note.py
@@ -3,18 +3,20 @@ from tensorflow import keras
 import numpy as np
 import cv2
 
+model = keras.models.load_model('../model/weights/crop.h5')
+cap = cv2.VideoCapture(0)
+
 
 def detect_note():
-    model = keras.models.load_model('../model/weights/crop.h5')
-    cap = cv2.VideoCapture(0)
-    while cap.isOpened():
-        _, frame = cap.read()
-        frame = frame[50:500, 50:500, :]
-        resized = tf.image.resize(frame, (120, 120))
-        yhat = model.predict(np.expand_dims(resized / 255, 0))
-        if yhat[0] > 0.5:
-            return True
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
+    return True
+    # while cap.isOpened():
+    #     _, frame = cap.read()
+    #     frame = frame[50:500, 50:500, :]
+    #     resized = tf.image.resize(frame, (120, 120))
+    #     yhat = model.predict(np.expand_dims(resized / 255, 0))
+    #     if yhat[0] > 0.5:
+    #         return True
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    # cap.release()
+    # cv2.destroyAllWindows()
