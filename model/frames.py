4bd8957471e985531463982ef17648c00113b26b
sayumithd <sayumi.20211479@iit.ac.lk>
2023-03-06 22:00:49 +0530

    split into frames

--- model/frames.py
+++ model/frames.py
@@ -7,3 +7,12 @@ video_files = ['10a.mp4', '10b.mp4', '10c.mp4', 'cent_10b.mp4', 'cent_50.mp4', '
 for video_file in video_files:
     # Open the video file
     cap = cv2.VideoCapture(video_file)
+
+    # Check if the video file was opened successfully
+    if not cap.isOpened():
+        print(f"Error opening video file: {video_file}")
+        continue
+
+    # Initialize a frame counter
+    frame_count = 0
+
