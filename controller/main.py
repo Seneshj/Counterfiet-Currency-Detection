473610f063cbc6327f7fab885c1ec37dedb587e8
sayumithd <sayumi.20211479@iit.ac.lk>
2023-03-09 13:26:06 +0530

    updated main

--- controller/main.py
+++ controller/main.py
@@ -2,6 +2,7 @@ import identify as i
 import fake_classifier as f
 import detect_note as d
 import utility
+import sumup
 
 while True:
     option = 0
@@ -23,6 +24,10 @@ while True:
             i.main()
             utility.voice_out(f.fake_classifier(i.get_value()))
     elif option == 3:
-        utility.voice_out("Note done")
+        while d.detect_note():
+            i.main()
+            sumup.calculate_sum_of_notes()
+            utility.voice_out("Identified notes:"+sumup.get_notes())
+            utility.voice_out("the sum of the note is"+sumup.get_sum_of_notes())
     elif option == 4:
         utility.voice_out("Have a nice day!")
