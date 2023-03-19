aac637264103b791cf575673016d22be6e28e6b3
yohanAbhishek <yohan.20200509@iit.ac.lk>
2023-03-19 15:20:53 +0530

    created new settings functions

--- controller/utility.py
+++ controller/utility.py
@@ -5,16 +5,54 @@ import pyttsx3
 print(">> Getting the camera ready >>")
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
+voice = 1
+speed = 150
+volume = 1.0
+
+
+def change_voice():
+    global voice
+    print(f"Current voice - {voice}")
+    new_voice = input("Please select 0(male) or 1(female): ")
+    if 0 <= int(new_voice) <= 5:
+        voice = int(new_voice)
+        voice_out("This is the new voice")
+    else:
+        voice_out("Invalid input")
+
+
+def change_speed():
+    global speed
+    print(f"Current speed - {speed/50}")
+    new_speed = input("Please select your speed from 0-5: ")
+    if 0 <= int(new_speed) <= 5:
+        speed = int(new_speed)*50
+        voice_out("This is the new speed")
+    else:
+        voice_out("Invalid input")
+
+
+def change_volume():
+    global volume
+    print(f"Current volume - {volume*5}")
+    new_volume = input("Please select your volume from 1-5: ")
+    if 0 < int(new_volume) <= 5:
+        volume = int(new_volume)/5
+        voice_out("This is the new volume")
+    else:
+        voice_out("Invalid input")
+
 
 def capture_image(name):
+    voice_out("Currency detected, now scanning")
     # Capture a frame from the camera
     ret, frame = cap.read()
 
     # Display the captured frame
-    cv2.imshow('frame', frame)
+    # cv2.imshow('frame', frame)
 
     # Check for user input to capture the image
-    if cv2.waitKey(1) & 0xFF == ord('c'):
+    if cv2.waitKey(5000):
         # Save the captured image
         cv2.imwrite(f'{name}.jpg', frame)
         cv2.destroyAllWindows()
@@ -27,11 +65,13 @@ def voice_out(phrase):
     # Initialize the engine
     engine = pyttsx3.init()
 
-    engine.setProperty('rate', 150)  # Change the rate to adjust the speed of speech
+    engine.setProperty('rate', speed)  # Change the rate to adjust the speed of speech
 
     # Set the voice properties (optional)
     voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[0].id)  # Change the index to switch voices
+    engine.setProperty('voice', voices[voice].id)  # Change the index to switch voices
+
+    engine.setProperty('volume', volume)  # Change the rate to adjust the volume of speech
 
     # Say the text
     engine.say(phrase)
