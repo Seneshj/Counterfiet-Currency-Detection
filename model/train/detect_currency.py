df2f1e7c297915d857bf46cf5efc98cefbbc9cfc
Seneshj <116113157+Seneshj@users.noreply.github.com>
2023-05-14 16:52:13 +0530

    Update detect_currency.py

--- model/train/detect_currency.py
+++ model/train/detect_currency.py
@@ -19,6 +19,20 @@ train = data.take(train_size)
 test = data.skip(train_size).take(test_size)
 val = data.skip(train_size+test_size).take(val_size)
 
+model = Sequential()
+
+model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
+model.add(MaxPooling2D())
+model.add(Conv2D(32, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Conv2D(16, (3,3), 1, activation='relu'))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
+
 logdir = 'logs'
 callback = tf.keras.callbacks.TensorBoard(log_dir = logdir)
 hist = model.fit(train,epochs = 10,validation_data = val,callbacks = [callback])
